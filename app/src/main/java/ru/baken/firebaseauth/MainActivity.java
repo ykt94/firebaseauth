aaeb3cf6b321c401748612ed5499add39c56742c
Alexander <ykt94@yandex.ru>
2020-04-25 20:45:48 +0900

    First commit

---
+++ app/src/main/java/ru/baken/firebaseauth/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.baken.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//fbauth-test-apr20     - id in Firebase
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
