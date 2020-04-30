f42e6ef329b4803eee17e701a96418b997f3b898
Alexander <ykt94@yandex.ru>
2020-04-30 18:19:29 +0900

    Added Firebase Phone Authorisation

--- app/src/main/java/ru/baken/firebaseauth/MainActivity.java
+++ app/src/main/java/ru/baken/firebaseauth/MainActivity.java
@@ -16,11 +16,17 @@ import android.widget.RelativeLayout;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthSettings;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.concurrent.TimeUnit;
 
 import ru.baken.firebaseauth.Models.User;
 
@@ -50,17 +56,32 @@ public class MainActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showRegisterWindow();
+                //showRegisterWindow();
+                //showRegisterPhone();
             }
         });
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showSignInWindow();
+                //showSignInWindow();
+                showSignInPhone();
             }
         });
     }
 
+    //enter to system
+    //registered - flag stored in phone
+    private void showSignInPhone() {
+        boolean registered = false; // temporary - TODO check procedure
+        if (registered) {
+            ;// enter to system
+        } else {
+            // register by phone
+            showPhoneRegister();
+        }
+    }
+
+    //авторизация по email
     private void showSignInWindow() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("Войти");
@@ -111,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    //регистрация по емайл
     private void showRegisterWindow() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("Зарегистрироваться");
@@ -170,6 +192,58 @@ public class MainActivity extends AppCompatActivity {
                                         });
                               }
                           });
+            }
+        });
+        dialog.show();
+    }
+
+    //show window for enter phone number and call register procedure
+    private void showPhoneRegister(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Зарегистрироваться");
+        dialog.setMessage("Введите все данные для регистрации");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_window = inflater.inflate(R.layout.register_window, null);
+        dialog.setView(register_window);
+
+        final MaterialEditText email = register_window.findViewById(R.id.emailField);
+        final MaterialEditText pass = register_window.findViewById(R.id.passField);
+        final MaterialEditText name = register_window.findViewById(R.id.nameField);
+        final MaterialEditText phone = register_window.findViewById(R.id.phoneField);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                /*
+                if(TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Введите вашу почту", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(name.getText().toString())) {
+                    Snackbar.make(root, "Введите вашу имя", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(pass.getText().toString().length() < 5) {
+                    Snackbar.make(root, "Введите пароль, длиной более 5 символов", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }*/
+
+                if(TextUtils.isEmpty(phone.getText().toString())) {
+                    Snackbar.make(root, "Введите ваш телефон", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                // регистрация пользователя
+                Intent intent = new Intent(MainActivity.this, VerifyPhoneActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.putExtra("mobile", phone.getText().toString().trim());
+                startActivity(intent);
             }
         });
         dialog.show();
